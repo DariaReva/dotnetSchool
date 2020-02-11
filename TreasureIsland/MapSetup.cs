b21b00aa512a6207cd3aa0c9f1ed759f585ffa9b
DReva <darya.reva@firstlinesoftware.com>
2020-02-11 22:13:36 +0300

    Добавьте файлы проекта.

---
+++ TreasureIsland/MapSetup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace TreasureIsland
+{
+    public class MapSetup
+    {    
+        public static void SetupConsoleWindow()
+        {   
+            Console.ForegroundColor = System.ConsoleColor.Red;
+            Console.BackgroundColor = System.ConsoleColor.White;
+
+            Console.Clear();
+        }
+
+        public static Point GetMaxMapPoint(List<Point> allMapCoordinates)
+        {
+            int maxX = allMapCoordinates[0].X;
+            int maxY = allMapCoordinates[0].Y;
+            for(int i = 0; i < allMapCoordinates.Count - 1; i++)
+            {
+                maxX = allMapCoordinates[i].X > allMapCoordinates[i + 1].X ? allMapCoordinates[i].X
+                                                                           : allMapCoordinates[i + 1].X;
+                maxY = allMapCoordinates[i].Y > allMapCoordinates[i + 1].Y ? allMapCoordinates[i].Y
+                                                                           : allMapCoordinates[i + 1].Y;
+            }
+
+            Point maxPoint = new Point(maxX, maxY);
+            return maxPoint;
+        }
+    }
+}
---
+++ TreasureIsland/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using TreasureIsland.BaseClassesAndInterfaces;
+
+namespace TreasureIsland
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            #region Necessary variables
+            MapEntity basement = new BaseEntity();
+            MapEntity bridge = new BridgeEntity();
+            MapEntity treasure = new TreasureEntity();
+            MapEntity water = new WaterEntity();
+
+            //Список точек, по которым нельзя прокладывать маршрут (река и точки базы)
+            List<Point> forbidden = new List<Point>();
+            //Список точек, которые нельзя перерисовывать (мост и клад)
+            List<Point> notRedraw = new List<Point>();
+            //Список выходов из базы
+            List<Point> baseExits = new List<Point>();
+            //Точка, где лежит клад
+            Point goal = new Point();
+            //Координаты всех сущностей карты
+            List<Point> allCoordinates = new List<Point>();
+            #endregion
+
+            //Подготовка файла
+            string filePath = Directory.GetCurrentDirectory() + "\\TestData\\Map.txt";
+            FileParser file = new FileParser(filePath);
+
+            string[] allLines = file.PrepareFile();
+            
+            //Подготовка консоли
+            MapSetup.SetupConsoleWindow();
+
+            //Визуализация сущностей карты
+            foreach (var line in allLines)
+            {
+                if (line.Split('(')[0].Equals(MapConsts.Base))
+                {
+                    List<Point> baseCoordinates = basement.Parse(line);
+                    basement.Print(baseCoordinates);
+                    baseExits = BaseEntity.GetExits(baseCoordinates);
+                    foreach (var point in BaseEntity.GetBasePoints(baseCoordinates))
+                    {
+                        forbidden.Add(point);
+                        allCoordinates.Add(point);
+                    }
+                    foreach (var point in baseExits)
+                        notRedraw.Add(point);
+                }
+            }
+
+            foreach (var line in allLines)
+            {
+                if (line.Split('(')[0].Equals(MapConsts.Treasure))
+                {
+                    List<Point> treasureCoordinates = treasure.Parse(line);
+                    treasure.Print(treasureCoordinates);
+                    goal = new Point(treasureCoordinates[0].X, treasureCoordinates[0].Y);
+                    notRedraw.Add(goal);
+                    allCoordinates.Add(goal);
+                }
+            }
+
+            foreach (var line in allLines)
+            {
+                if (line.Split('(')[0].Equals(MapConsts.Water))
+                {
+                    List<Point> waterCoordinates = water.Parse(line);
+                    water.Print(waterCoordinates);
+                    foreach (var point in WaterEntity.GetWaterPoints(waterCoordinates))
+                    {
+                        forbidden.Add(point);
+                        allCoordinates.Add(point);
+                    }
+                }
+            }
+
+            foreach (var line in allLines)
+            {
+                if (line.Split('(')[0].Equals(MapConsts.Bridge))
+                {
+                    List<Point> bridgeCoordinates = bridge.Parse(line);
+                    bridge.Print(bridgeCoordinates);
+                    forbidden.Remove(bridgeCoordinates[0]);
+                    notRedraw.Add(new Point(bridgeCoordinates[0].X, bridgeCoordinates[0].Y));
+                    allCoordinates.Add(new Point(bridgeCoordinates[0].X, bridgeCoordinates[0].Y));
+                }
+
+            }
+
+            //Поиск границ карты
+            Point maxPoint = MapSetup.GetMaxMapPoint(allCoordinates);
+
+            //Отрисовка пути
+            PathCalculation.PrintPath(baseExits, goal, forbidden, notRedraw, maxPoint);
+            
+            Console.ReadLine();
+        }
+	}
+}
